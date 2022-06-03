dc54b22cbd085007e3933875a5929bda15015778
Nyckzin10 <backtonarnia@hotmail.com>
2022-06-03 18:02:21 -0300

    "Projeto Finalizado"

---
+++ js/js.js
@@ -0,0 +1,24 @@
+let onclik = ('Validação')
+
+const tel = 0;
+
+function onclik {
+    if(onclik){
+        onclik >= 'hrfe'
+    }else{
+        return base;
+    }
+}
+
+
+
+function tel {
+    if(tel)`nome{'nome'}/email{'email'}/planodesaude{'plano'}/idade{idade}/tipodeplano{tipo-plano}`{
+        else{
+            return
+            console.log('Seja bem vindo ao atendimento da SaúdeFuse')
+        }
+    }
+}
+
+console.log++=={{ReadableStreamDefaultController}}
